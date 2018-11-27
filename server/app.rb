94a3bd5f65535e2de20690b9cf61df1ca15df75b
vedranvinko <vinkovedran@gmail.com>
2018-11-27 12:51:10 +0100

    basic setup

--- server/app.rb
+++ server/app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+
+configure do
+  enable _cross_origin
+end
 
 get '/' do
 
+end
+
+get '*' do
+  response.headers['Allow'] = 'GET, POST, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  200
 end
